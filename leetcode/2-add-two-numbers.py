ce5820282248e2ec04897cc64c5e5b0b386fec52
dariusfratila <dariusfratila818@gmail.com>
2024-10-25 15:40:23 +0300

    fixed: solution 2

--- leetcode/2-add-two-numbers.py
+++ leetcode/2-add-two-numbers.py
@@ -12,14 +12,26 @@ space: O(n)
 
 
 class Solution:
-    def hasCycle(self, head: Optional[ListNode]) -> bool:
-        current = head
-        hashSet = set()
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        current = dummy
 
-        while current:
-            if current in hashSet:
-                return True
-            hashSet.add(current)
+        carry = 0
+        while l1 or l2:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+            sumElements = val1 + val2 + carry
+
+            carry = sumElements // 10
+            sumElements %= 10
+
+            current.next = ListNode(sumElements)
             current = current.next
 
-        return False
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+
+        if carry:
+            current.next = ListNode(carry)
+
+        return dummy.next
