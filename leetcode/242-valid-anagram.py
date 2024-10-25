5029eae863a2a17c42aa7dbe682aa73fa5cb8836
dariusfratila <dariusfratila818@gmail.com>
2024-10-25 12:12:11 +0300

    feat: solved 242

---
+++ leetcode/242-valid-anagram.py
@@ -0,0 +1,12 @@
+"""
+@complexity
+time: O(n)
+space: O(n)
+"""
+
+from collections import Counter
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return Counter(s) == Counter(t)
