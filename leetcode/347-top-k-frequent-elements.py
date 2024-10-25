922144862d0bc66c395e0f4c0ac8ee65b1310514
dariusfratila <dariusfratila818@gmail.com>
2024-10-25 12:04:52 +0300

    feat: solved 347

---
+++ leetcode/347-top-k-frequent-elements.py
@@ -0,0 +1,24 @@
+"""
+@complexity
+time: O(n)
+space: O(n)
+"""
+
+from collections import Counter
+from typing import List
+
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freq_hash = Counter(nums)
+        count = [[] for _ in range(len(nums) + 1)]
+
+        for num, freq in freq_hash.items():
+            count[freq].append(num)
+
+        output = []
+        for index in range(len(nums), 0, -1):
+            for j in count[index]:
+                output.append(j)
+                if len(output) == k:
+                    return output
