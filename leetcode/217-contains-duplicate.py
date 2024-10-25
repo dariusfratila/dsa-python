e296929bbf069ad053c8afa6b2ecf16dcdcc21f9
dariusfratila <dariusfratila818@gmail.com>
2024-10-25 12:14:28 +0300

    feat: solved 217

---
+++ leetcode/217-contains-duplicate.py
@@ -0,0 +1,15 @@
+"""
+@complexity
+time: O(n)
+space: O(n)
+"""
+
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+
+        return False
