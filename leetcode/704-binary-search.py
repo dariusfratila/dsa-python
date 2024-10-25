6e4600ecf2a0c71003d4f0b25bb4bbcf986b285f
dariusfratila <dariusfratila818@gmail.com>
2024-10-25 12:41:05 +0300

    feat: solved 704

---
+++ leetcode/704-binary-search.py
@@ -0,0 +1,19 @@
+"""
+@complexity
+time: O(log n)
+space: O(1)
+"""
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            elif nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+        return -1
