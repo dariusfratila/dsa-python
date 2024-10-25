a0fac1d6dbad7316962e3a90bb221d403894e16d
dariusfratila <dariusfratila818@gmail.com>
2024-10-25 12:29:22 +0300

    feat: solved 155

---
+++ leetcode/155-min-stack.py
@@ -0,0 +1,25 @@
+"""
+@complexity
+time: O(1) for each operation
+space: O(n)
+"""
+
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        val = min(val, self.minStack[-1] if self.minStack else val)
+        self.minStack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minStack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
