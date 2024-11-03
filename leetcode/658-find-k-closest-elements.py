2ef7973bb6cc7da29f360cd27597d2446f14b8ff
dariusfratila <dariusfratila818@gmail.com>
2024-11-03 15:59:46 +0200

    extra: solved 658

--- leetcode/658-find-k-closest-elements.py
+++ leetcode/658-find-k-closest-elements.py
@@ -1,7 +1,11 @@
 """
-@complexity
+@complexity sliding window
 time: O(n)
 space: O(k)
+
+@complexity two pointers
+time: O(n)
+space: O(1)
 """
 
 
@@ -14,16 +18,31 @@ class Solution(object):
         :rtype: List[int]
         """
 
-        resArray = arr[:k]
-        l = 0
+        # sliding window
 
-        for r in range(k, len(arr)):
-            differenceL = abs(arr[l] - x)
-            differenceR = abs(arr[r] - x)
+        # resArray = arr[:k]
+        # l = 0
 
-            if differenceL > differenceR:
-                resArray.pop(0)
-                resArray.append(arr[r])
+        # for r in range(k, len(arr)):
+        #     differenceL = abs(arr[l] - x)
+        #     differenceR = abs(arr[r] - x)
+
+        #     if differenceL > differenceR:
+        #         resArray.pop(0)
+        #         resArray.append(arr[r])
+        #         l += 1
+
+        # return resArray
+
+        # ----
+
+        # two pointers
+
+        l, r = 0, len(arr) - 1
+        while r - l >= k:
+            if abs(arr[l] - x) > abs(arr[r] - x):
                 l += 1
+            else:
+                r -= 1
 
-        return resArray
+        return arr[l:r + 1]
