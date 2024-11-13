8d280f073b6edaff78cdd0034f94a03e389bf7a7
dariusfratila <dariusfratila818@gmail.com>
2024-11-13 23:00:52 +0200

    feat: solved 61

---
+++ leetcode/61-rotate-list.py
@@ -0,0 +1,43 @@
+"""
+@complexity
+time: O(n)
+space: O(1)
+"""
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+
+class Solution(object):
+    def rotateRight(self, head, k):
+        """
+        :type head: Optional[ListNode]
+        :type k: int
+        :rtype: Optional[ListNode]
+        """
+
+        if not head:
+            return head
+
+        length, tail = 1, head
+
+        while tail.next:
+            tail = tail.next
+            length += 1
+
+        k = k % length
+        if k == 0:
+            return head
+
+        curr = head
+        for index in range(length - k - 1):
+            curr = curr.next
+
+        newHead = curr.next
+        curr.next = None
+        tail.next = head
+
+        return newHead
