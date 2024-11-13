45ab4bda5283c98b2c7787bc72f6716b24419e14
dariusfratila <dariusfratila818@gmail.com>
2024-11-13 13:46:42 +0200

    feat: 160 update

--- leetcode/160-intersection-of-two-linked-lists.py
+++ leetcode/160-intersection-of-two-linked-lists.py
@@ -5,17 +5,46 @@ space: O(1)
 """
 
 # Definition for singly-linked list.
-# class ListNode:
+# class ListNode(object):
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
 
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
 
-class Solution:
-    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        l1, l2 = headA, headB
-        while l1 != l2:
-            l1 = l1.next if l1 else headB
-            l2 = l2.next if l2 else headA
+        list1, list2 = headA, headB
 
-        return l1
+        count_list1, count_list2 = 0, 0
+        while list1:
+            count_list1 += 1
+            list1 = list1.next
+
+        while list2:
+            count_list2 += 1
+            list2 = list2.next
+
+        listLonger, listShorter = 0, 0
+        difference = 0
+        if count_list1 > count_list2:
+            listLonger = headA
+            difference = count_list1 - count_list2
+            listShorter = headB
+        else:
+            listLonger = headB
+            difference = count_list2 - count_list1
+            listShorter = headA
+
+        while difference:
+            listLonger = listLonger.next
+            difference -= 1
+
+        while listLonger != listShorter:
+            listLonger = listLonger.next
+            listShorter = listShorter.next
+
+        return listLonger
