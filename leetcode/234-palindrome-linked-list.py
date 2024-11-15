7f0ebe3a1c418fad1066c208c597d49da24f4724
dariusfratila <dariusfratila818@gmail.com>
2024-11-15 14:00:42 +0200

    feat: solved 234

---
+++ leetcode/234-palindrome-linked-list.py
@@ -0,0 +1,41 @@
+"""
+@complexity
+time: O(n)
+space: O(1)
+"""
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+
+class Solution(object):
+    def isPalindrome(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: bool
+        """
+
+        slow, fast = head, head
+
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+
+        prev = None
+        while slow:
+            tmp = slow.next
+            slow.next = prev
+            prev = slow
+            slow = tmp
+
+        left, right = head, prev
+        while right:
+            if left.val != right.val:
+                return False
+            right = right.next
+            left = left.next
+
+        return True
