7191e3fa1ed1cd8ea6d54897a15365ef608b4601
dariusfratila <dariusfratila818@gmail.com>
2024-11-21 01:38:15 +0200

    feat: solved 344-1

--- leetcode/344-reverse-string-1.py
+++ leetcode/344-reverse-string-1.py
@@ -15,4 +15,5 @@ class Solution(object):
         l, r = 0, len(s) - 1
         while l < r:
             s[l], s[r] = s[r], s[l]
-            l, r = l + 1, r - 1
+            l = l + 1
+            r = r - 1
