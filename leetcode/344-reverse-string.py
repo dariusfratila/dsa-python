6eff29d6b731cc5d3500e1617737d6ace96885ff
dariusfratila <dariusfratila818@gmail.com>
2024-11-20 19:41:01 +0200

    feat: solved 344

--- leetcode/344-reverse-string.py
+++ leetcode/344-reverse-string.py
@@ -1,7 +1,7 @@
 """
 @complexity
 time: O(n)
-space: O(n)
+space: O(1)
 """
 
 
@@ -12,9 +12,7 @@ class Solution(object):
         :rtype: None Do not return anything, modify s in-place instead.
         """
 
-        def reverse(l, r):
-            if l < r:
-                s[l], s[r] = s[r], s[l]
-                reverse(l + 1, r - 1)
-
-        reverse(0, len(s) - 1)
+        l, r = 0, len(s) - 1
+        while l < r:
+            s[l], s[r] = s[r], s[l]
+            l, r = l + 1, r - 1
