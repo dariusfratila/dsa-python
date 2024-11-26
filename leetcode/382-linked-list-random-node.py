641c0313fe20db1eeb82ccdb926c70556d5cf088
dariusfratila <dariusfratila818@gmail.com>
2024-11-26 16:33:19 +0200

    feat: solved 382

---
+++ leetcode/382-linked-list-random-node.py
@@ -0,0 +1,33 @@
+"""
+@complexity
+time: O(n)
+space: O(1)
+"""
+
+
+class Solution(object):
+
+    def __init__(self, head):
+        """
+        :type head: Optional[ListNode]
+        """
+
+        self.head = head
+
+    def getRandom(self):
+        """
+        :rtype: int
+        """
+
+        result = None
+        count = 0.0
+        current = self.head
+
+        while current:
+            count += 1
+            rd = random.random()
+            if rd < (1 / count):
+                result = current.val
+            current = current.next
+
+        return result
