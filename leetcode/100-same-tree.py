0e9104a97951f9e97ada7e0fe15e04448c59d897
dariusfratila <dariusfratila818@gmail.com>
2024-11-28 01:48:00 +0200

    feat: solved 100

---
+++ leetcode/100-same-tree.py
@@ -0,0 +1,28 @@
+"""
+@complexity
+time: O(n)
+space: O(n)
+"""
+
+
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: Optional[TreeNode]
+        :type q: Optional[TreeNode]
+        :rtype: bool
+        """
+
+        if not p and not q:
+            return True
+
+        if not p or not q:
+            return False
+
+        if p.val != q.val:
+            return False
+
+        leftTree = self.isSameTree(p.left, q.left)
+        rightTree = self.isSameTree(p.right, q.right)
+
+        return leftTree and rightTree
