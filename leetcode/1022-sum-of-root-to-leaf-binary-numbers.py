7a5d4101ddde1c961189c4ab37b92113cebdfcef
dariusfratila <dariusfratila818@gmail.com>
2024-12-17 02:46:18 +0200

    feat: solved 1022 | bits solution

--- leetcode/1022-sum-of-root-to-leaf-binary-numbers.py
+++ leetcode/1022-sum-of-root-to-leaf-binary-numbers.py
@@ -6,6 +6,29 @@ space: O(h)
 
 class Solution:
     def sumRootToLeaf(self, root: Optional[TreeNode]) -> int:
+        stack = [(root, root.val)]
+        total_sum = 0
+        while stack:
+            node, node_val = stack.pop()
+            if not node.left and not node.right:
+                total_sum += node_val
+            if node.left:
+                stack.append([node.left, node_val << 1 | node.left.val])
+            if node.right:
+                stack.append([node.right, node_val << 1 | node.right.val])
+
+        return total_sum
+
+
+"""
+@complexity
+time: O(n)
+space: O(h)
+"""
+
+from collections import deque
+class Solution:
+    def sumRootToLeaf(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
 
