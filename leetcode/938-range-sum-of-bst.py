e319f88b3879cd8ccc332cf44a71f76c66bd1f94
dariusfratila <dariusfratila818@gmail.com>
2024-12-19 00:57:17 +0200

    feat: solved 938

---
+++ leetcode/938-range-sum-of-bst.py
@@ -0,0 +1,23 @@
+"""
+@complexity
+time: O(n)
+space: O(h)
+"""
+
+
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        deq = deque([root])
+        total_sum = 0
+        while deq:
+            for _ in range(len(deq)):
+                node = deq.popleft()
+                if low <= node.val <= high:
+                    total_sum += node.val
+
+                if node.left:
+                    deq.append(node.left)
+                if node.right:
+                    deq.append(node.right)
+
+        return total_sum
