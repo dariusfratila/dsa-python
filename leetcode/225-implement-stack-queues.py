7b80b68f4249257cb16c5f347e945f9f43f8b8f4
dariusfratila <dariusfratila818@gmail.com>
2025-01-11 00:09:37 +0200

    solved 225

---
+++ leetcode/225-implement-stack-queues.py
@@ -0,0 +1,25 @@
+"""
+@complexity
+time: O(1)
+space: O(n)
+"""
+
+
+class MyStack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+
+    def pop(self) -> int:
+        if not self.empty():
+            return self.stack.pop()
+
+    def top(self) -> int:
+        if not self.empty():
+            return self.stack[-1]
+
+    def empty(self) -> bool:
+        return len(self.stack) == 0
