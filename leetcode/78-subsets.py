e7a1947ba2712032ec3feea2387e3253f3fb2d30
dariusfratila <dariusfratila818@gmail.com>
2025-01-21 14:05:08 +0200

    solved 78

---
+++ leetcode/78-subsets.py
@@ -0,0 +1,25 @@
+"""
+@complexity
+time: O(2^n)
+space: O(n)
+"""
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        subset = []
+
+        def dfs(index):
+            if index == len(nums):
+                res.append(subset.copy())
+                return
+
+            subset.append(nums[index])
+            dfs(index + 1)
+
+            subset.pop()
+            dfs(index + 1)
+
+        dfs(0)
+        return res
