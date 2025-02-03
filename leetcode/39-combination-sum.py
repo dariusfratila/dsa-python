361c06f3312c5255626be63a06e1f7b0362d629e
dariusfratila <dariusfratila818@gmail.com>
2025-02-03 13:57:57 +0200

    update 39

--- leetcode/39-combination-sum.py
+++ leetcode/39-combination-sum.py
@@ -1,3 +1,10 @@
+""""
+@complexity
+time: O(2^n)
+space: O(n)
+"""
+
+
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
@@ -5,13 +12,13 @@ class Solution:
         def dfs(index, current_list, total_sum):
             if total_sum == target:
                 res.append(current_list.copy())
-                return 
+                return
 
             if index >= len(candidates) or total_sum > target:
                 return
 
             current_list.append(candidates[index])
-            
+
             dfs(index, current_list, candidates[index] + total_sum)
 
             current_list.pop()
