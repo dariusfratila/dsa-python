f353c6c3e8d47b40fefb2b0bdb43e2b5d4aee3a9
dariusfratila <dariusfratila818@gmail.com>
2025-02-20 16:19:38 +0200

    improve 994

--- leetcode/994-rotting-oranges.py
+++ leetcode/994-rotting-oranges.py
@@ -4,37 +4,38 @@ time: O(n)
 space: O(n)
 """
 
-
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
         ROWS, COLS = len(grid), len(grid[0])
-        queue = deque()
 
-        def isValid(i, j):
-            return 0 <= i < ROWS and 0 <= j < COLS and grid[i][j] == 1
+        queue = deque()
 
         fresh = 0
         for i in range(ROWS):
             for j in range(COLS):
                 if grid[i][j] == 2:
-                    queue.append((i, j, 0))
+                    queue.append((i, j))
                 elif grid[i][j] == 1:
                     fresh += 1
+
+        def isValid(i, j):
+            return 0 <= i < ROWS and 0 <= j < COLS and grid[i][j] == 1
 
         if fresh == 0:
             return 0
 
-        minutes = 0
-        while queue:
-            x, y, time = queue.popleft()
-            minutes = max(minutes, time)
+        time = 0
+        while queue and fresh:
+            for _ in range(len(queue)):
+                i, j = queue.popleft()
+                for di, dj in directions:
+                    new_i, new_j = i + di, j + dj
+                    if isValid(new_i, new_j):
+                        grid[new_i][new_j] = 2
+                        fresh -= 1
+                        queue.append((new_i, new_j))
 
-            for dx, dy in directions:
-                new_x, new_y = x + dx, y + dy
-                if isValid(new_x, new_y):
-                    grid[new_x][new_y] = 2
-                    fresh -= 1
-                    queue.append((new_x, new_y, time + 1))
+            time += 1
 
-        return minutes if fresh == 0 else -1
+        return time if fresh == 0 else -1
